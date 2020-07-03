c5dc03823a61877f4bdaf9e7b75bb6d93e983fbc
Lucas Sales <lucassales398@gmail.com>
2020-07-02 23:20:04 -0300

    continuação componente Button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,4 +1,18 @@
 import React from "react";
 import "./Button.css";
 
-export default (props) => <button className="button">{props.label}</button>;
+export default (props) => {
+  return (
+    <button
+      onClick={() => props.click && props.click(props.label)}
+      className={`
+          button
+          ${props.operation ? "operation" : ""}
+          ${props.double ? "double" : ""}
+          ${props.triple ? "triple" : ""}
+        `}
+    >
+      {props.label}
+    </button>
+  );
+};
